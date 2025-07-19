f9ac7d63b5b8a368dc85c3852ad8e20c697b68f9
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-19 14:04:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def get_help_text():
         ".stopdelayspam → Hentikan semua spam\n"
         ".restart → Restart bot\n"
         ".gcast teks → Kirim ke semua chat"
+        ".delayspamf → Delayspam Forward\n"
     )
 
 def get_help_page(page=1):
@@ -44,6 +45,7 @@ def get_help_page(page=1):
         return InlineKeyboardMarkup([
             [InlineKeyboardButton("Restart", callback_data="restart"),
              InlineKeyboardButton("Gcast", callback_data="gcast")],
+             InlineKeyboardButton("Delayspamf", callback_data="delayspamf")],
             [InlineKeyboardButton("⬅️", callback_data="help_page1")]
         ])
 
@@ -64,6 +66,33 @@ async def spam_handler(client, message):
     text = args[2]
     for _ in range(count):
         await message.reply(text)
+
+@userbot.on_message(filters.me & filters.command("delayspamf", prefixes=".")
+async def delayspam_forward(client, message):
+    try:
+        args = message.text.split()
+        if len(args) < 5:
+            await message.reply("Gunakan format:\n`/delayspamf dari_chat_id msg_id jumlah delay`")
+            return
+
+        from_chat = int(args[1]) if args[1].lstrip("-").isdigit() else args[1]
+        msg_id = int(args[2])
+        jumlah = int(args[3])
+        delay = float(args[4])
+
+        to_chat = message.chat.id
+        await message.delete()
+
+        for _ in range(jumlah):
+            await client.forward_messages(
+                chat_id=to_chat,
+                from_chat_id=from_chat,
+                message_ids=msg_id
+            )
+            await asyncio.sleep(delay)
+
+    except Exception as e:
+        await message.reply(f"❌ Error: {e}")
 
 @userbot.on_message(filters.me & filters.command("delayspam", prefixes="."))
 async def delayspam_handler(client, message):
